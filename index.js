55cf3b9d3115fc5f3800d1383911b6c2ec177ce2
Elijah <bundielly@gmail.com>
2022-06-06 19:37:26 +0300

    Arithmetic equations

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+const max = Math.max(1,3,5,7,9,20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
